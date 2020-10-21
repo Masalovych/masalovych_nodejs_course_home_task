582f0791d138143500e98a84945a81a21cf7efc7
dmasalovych <dmasalovych@gmail.com>
2020-10-21 18:36:16 +0300

    NodeJs Course: Home Task #1
    - Create app.js, config.js, logger.js ​
    - Logger should be an object that wraps console.log methods (log, debug, error, warn) ​
    - App should require config and logger. ​
    - App should run dumb server which logs hello world on each request (can be found on slide 7) ​
    - When server is up there should be a log message “Listening on port <PORT>” ​
    - Config should export object with roperties: PORT, ENV ​
    - Port should come from env vars. ​
    - Env should be passed through as cli argument ​
    (format is the following: -arg=val) ​
    - Example of running: PORT=3000 node app.js -env=dev

---
+++ config.js
@@ -0,0 +1,24 @@
+
+const DEFAULT_NODE_PORT = 3000;
+const DEFAULT_ENV = 'development';
+
+const availableArgs = parseNodeArgs(process.argv);
+
+function parseNodeArgs(nodeArgs) {
+  const requiredArgLength = 2;
+  const argsArr = nodeArgs.slice(requiredArgLength);
+  const argRegExp = /^-(\w+)=(\w+)/;
+
+  return argsArr.reduce((acc, item) => {
+    if(argRegExp.test(item)) {
+      const matches = item.match(argRegExp)
+      acc[matches[1]] = matches[2];
+    }
+    return acc;
+  }, {});
+}
+
+module.exports = {
+  PORT: process.env.NODE_PORT || DEFAULT_NODE_PORT,
+  ENV: availableArgs.env || DEFAULT_ENV
+};
